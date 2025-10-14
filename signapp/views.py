d73d2c02cc245a3e24951479501e38cbc6ce940b
Pauwiiii <bern.github@gmail.com>
2025-10-14 11:56:46 +0800

    Lab 4

--- signapp/views.py
+++ signapp/views.py
@@ -21,3 +21,26 @@ def list_user(request):
     users = SignUpRegistration.objects.all()
     serializer = SignUpRegistrationSerializer(users, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail(request, pk):
+    try:
+        user = SignUpRegistration.objects.get(pk=pk)
+    except SignUpRegistration.DoesNotExist:
+        return Response({"error": "Not Found"}, status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = SignUpRegistrationSerializer(user)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = SignUpRegistrationSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            return Response(serializer.data)
+        
+        else:
+            print("Serializer error", serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)    
